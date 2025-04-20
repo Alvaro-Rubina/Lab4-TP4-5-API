3ee64a86a4ed83f4133ff0bf37022e56ebbdc61e
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-20 20:04:25 -0300

    Creado ControllerAdvice

---
+++ src/main/java/org/spdgrupo/lab4tp45api/config/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.spdgrupo.lab4tp45api.config.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
