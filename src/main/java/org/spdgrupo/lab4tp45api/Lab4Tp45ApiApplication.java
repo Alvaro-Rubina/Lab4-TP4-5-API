07944ec3838a8b1324accd13c0ca27757257e899
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-20 19:03:50 -0300

    Primer commit

---
+++ src/main/java/org/spdgrupo/lab4tp45api/Lab4Tp45ApiApplication.java
@@ -0,0 +1,13 @@
+package org.spdgrupo.lab4tp45api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4Tp45ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4Tp45ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spdgrupo/lab4tp45api/Lab4Tp45ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spdgrupo.lab4tp45api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4Tp45ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
