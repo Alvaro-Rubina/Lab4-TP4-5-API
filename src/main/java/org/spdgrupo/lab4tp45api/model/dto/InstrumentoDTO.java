a7143edfe528ea4ba2d7582e3487eebe814d7160
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-20 21:18:04 -0300

    Agregadas validaciones a los DTO

--- src/main/java/org/spdgrupo/lab4tp45api/model/dto/InstrumentoDTO.java
+++ src/main/java/org/spdgrupo/lab4tp45api/model/dto/InstrumentoDTO.java
@@ -1,5 +1,6 @@
 package org.spdgrupo.lab4tp45api.model.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @NoArgsConstructor
@@ -10,14 +11,32 @@ import lombok.*;
 public class InstrumentoDTO {
 
     private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
+
+    @NotBlank(message = "La marca es obligatoria")
     private String marca;
+
+    @NotBlank(message = "El modelo es obligatorio")
     private String modelo;
+
+    @NotBlank(message = "La descripcion es obligatoria")
     private String descripcion;
+
+    @NotBlank(message = "La imagen es obligatoria")
     private String imagen;
+
+    @NotNull(message = "El precio es obligatorio")
+    @Positive(message = "El precio no puede ser negativo")
     private Double precio;
+
+    @NotBlank(message = "El costo de envio es obligatorio")
     private String costoEnvio;
+
+    @NotNull(message = "La cantidad vendida es obligatoria")
+    @PositiveOrZero(message = "La cantidad vendida debe ser positiva o cero")
     private int cantidadVendida;
-    private Boolean activo;
+    private boolean activo;
     private CategoriaInstrumentoDTO categoria;
 }
