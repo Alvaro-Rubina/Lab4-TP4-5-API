b4cfb9fdef019432e14e5356b71ccb8c6b392cfd
Alvaro Rubina <alvarorubina132@gmail.com>
2025-05-14 08:21:33 -0300

    Añadido caso para manejar excepciones de Enums
    En GlobalEsceptionHandler

--- src/main/java/org/spdgrupo/lab4tp45api/config/exception/GlobalExceptionHandler.java
+++ src/main/java/org/spdgrupo/lab4tp45api/config/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package org.spdgrupo.lab4tp45api.config.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.mercadopago.exceptions.MPApiException;
 import com.mercadopago.exceptions.MPException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,6 +28,23 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MPApiException.class)
     public ResponseEntity<String> handleMPApiException(MPApiException ex) {
         return new ResponseEntity<>("Error en la API de MercadoPago: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        if (ex.getCause() instanceof InvalidFormatException) {
+            InvalidFormatException cause = (InvalidFormatException) ex.getCause();
+            if (cause.getTargetType() != null && cause.getTargetType().isEnum()) {
+                String valoresValidos = Arrays.stream(cause.getTargetType().getEnumConstants())
+                        .map(Object::toString)
+                        .collect(Collectors.joining(", "));
+                return new ResponseEntity<>(
+                        "Valor inválido: '" + cause.getValue() + "'. Los valores permitidos son: [" + valoresValidos + "]",
+                        HttpStatus.BAD_REQUEST
+                );
+            }
+        }
+        return new ResponseEntity<>("Error en el formato del JSON: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
